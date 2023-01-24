0fa4c9458eba71e64bb298927b7ed534e710065a
Isfrankle Souza <isfrankle.silva@grupopan.com>
2023-01-24 11:45:30 -0300

    feat(backend): creating db tables

--- Server/src/routes.ts
+++ Server/src/routes.ts
@@ -2,9 +2,7 @@ import { FastifyInstance } from "fastify"
 import { prisma } from "./lib/prisma"
 
 export async function appRoutes(app: FastifyInstance){
-    app.get('/hello', async () => {
-        const habits = await prisma.habit.findMany()
-           
-        return habits
+    app.post('/habits', async (request) => {
+        const { title, weekDays } = request.body
     })
 }
